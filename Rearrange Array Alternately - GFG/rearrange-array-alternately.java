30432f0a2192ef9ab1ba18ef5b8083aa116118d6
Sanket Srivastava <118609640+Algo-Sanket@users.noreply.github.com>
2022-12-01 01:43:16 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public static void rearrange(long arr[], int n)
+    {
+    long temp[] = arr.clone();
+        int low = 0, high = n - 1;
+        boolean flag = true;
+        for (int i = 0; i < n; i++) 
+        {
+            if (flag)
+                arr[i] = temp[high--];
+            else
+                arr[i] = temp[low++];
+ 
+            flag = !flag;
+        }
+    }
+}
