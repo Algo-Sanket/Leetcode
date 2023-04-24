1c73859ed91643c66ba09d9747d068cf286799e8
Sanket Srivastava <118609640+Algo-Sanket@users.noreply.github.com>
2023-04-24 09:45:14 +0530

    Added solution - LeetHub

--- Find Transition Point - GFG/find-transition-point.java
+++ Find Transition Point - GFG/find-transition-point.java
@@ -23,14 +23,20 @@ class Sorted_Array {
 class GfG {
     int transitionPoint(int arr[], int n)
     {
-        for(int i=0;i<n;i++)
-        {
-        if(arr[i]==1)
-        {
-            return i;
+int lb=0;
+int ub=n-1;
+  while (lb <= ub) {
+            int mid = (lb + ub) / 2;
+            if (arr[mid] == 0)
+                lb = mid + 1;
+            else if (arr[mid] == 1) {
+                if (mid == 0
+                    || (mid > 0 &&
+                       arr[mid - 1] == 0))
+                    return mid;
+                ub = mid - 1;
+            }
         }
-        
+        return -1;
     }
-    return -1;
-}
 }
