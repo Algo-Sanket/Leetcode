d239aa793de118c0c33b8b25aa4e25192888c3cf
Sanket Srivastava <118609640+Algo-Sanket@users.noreply.github.com>
2023-04-28 02:59:29 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (31.79%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,9 +1,9 @@
-public class Solution {
-    public int bulbSwitch(int n) {
-        int count = 0;
-        for (int i = 1; i * i <= n; i++) {
-            count++;
-        }
-        return count;
+public class Solution 
+{
+    public int bulbSwitch(int n) 
+    {
+ return (int)Math.sqrt(n);
     }
 }
+
+
