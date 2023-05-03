4020db18825d3cb7a5deb4310a021272de523c33
Sanket Srivastava <118609640+Algo-Sanket@users.noreply.github.com>
2023-05-03 23:00:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.84%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -3,7 +3,12 @@ class Solution
 public:
     int bulbSwitch(int n)
     {
-    return sqrt(n);
+        int count=0;
+    for(int i=1;i*i<=n;i++)
+    {
+        count++;
+    }
+        return count;
     }
 };
        
