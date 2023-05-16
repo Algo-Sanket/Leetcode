33dbc45b2ff70625f1d707bbf3ae7a848329644e
Sanket Srivastava <118609640+Algo-Sanket@users.noreply.github.com>
2023-05-16 14:07:16 +0530

    Time: 42 ms (80.30%), Space: 43.6 MB (54.55%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public int numberOfWays(String s) 
+    {
+    long res = 1;
+    int j = 0;
+    int k = 0;
+    long mod = (long)1e9 + 7;
+    for (int i = 0; i < s.length(); i++) 
+    {
+        if (s.charAt(i) == 'S')
+        {
+            k++;
+            if (k > 2 && k % 2 == 1) 
+            {
+                res = (res * (i - j)) % mod;
+            }
+            j = i;
+        }
+    }
+
+    if (k % 2 == 0 && k > 0)
+    {
+        return (int) res;
+    } else 
+    {
+        return 0;
+    }
+}
+}
